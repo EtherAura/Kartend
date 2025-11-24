ba809f218263ec6d16d9448e6efc9d27422abecc
EtherAura <etheraura@protonmail.com>
2025-11-24 09:56:24 -0500

    Stop tracking build artifacts and add .gitignore

--- build/release/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_kartend_FILE /home/carl/Documents/Programming/Kartend/build/release/kartend)
-set(__QT_DEPLOY_TARGET_kartend_TYPE EXECUTABLE)
--- build/release/CMakeFiles/kartend.dir/kartend_autogen/7WA7TAYJOR/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/kartend.dir/kartend_autogen/7WA7TAYJOR/qrc_resources.cpp.o: \
-  /home/carl/Documents/Programming/Kartend/build/release/kartend_autogen/7WA7TAYJOR/qrc_resources.cpp
